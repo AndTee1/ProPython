95d82b00b3ef68d980cc118e33dfe4df6a020597
Admin <lhx2911.pp@gmail.com>
2020-06-19 00:15:19 +0700

    update3

--- MVC/test.py
+++
@@ -1,8 +0,0 @@
-from MVC.cus import Customer
-def isObject():
-    cus=Customer(NameCustomer=None,PhoneCustomer=None,CMNDCustomer=None,UsedTime=None)
-    return cus
-def getModel():
-    gre=isObject()
-    print(gre)
-getModel()
